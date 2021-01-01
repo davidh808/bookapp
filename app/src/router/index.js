ca074416006f91000e5772e9e813a3e2950f3be7
David Hodges <davidhodges808@gmail.com>
2021-01-01 10:36:30 -0600

    node cleanup and dockerized

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import BookApp from '../views/BookApp.vue';
 
 Vue.use(VueRouter);
 
@@ -11,9 +12,9 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue'),
+    path: '/bookapp',
+    name: 'Book App',
+    component: BookApp,
   },
 ];
 
---
+++ app/src/views/BookApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bookapp">
+    <Books msg="This is the Book App page!"/>
+  </div>
+</template>
+
+<script>
+import Books from '@/components/Books.vue';
+
+export default {
+  name: 'BookApp',
+  components: {
+    Books,
+  },
+};
+</script>
